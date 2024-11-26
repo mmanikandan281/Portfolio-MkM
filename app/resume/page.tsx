06f14f58238e9bd832858cbdd0ce3a33769d4895
Noor_Mohammad <noor.jsdivs@gmail.com>
2024-11-27 00:57:54 +0600

    updated

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+"use client";
+import Container from "@/components/Container";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { motion } from "framer-motion";
 
 const ResumePage = () => {
-  return <div>ResumePage</div>;
+  return (
+    <Container className="py-10">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{
+          opacity: 1,
+          transition: { delay: 2.4, duration: 0.4, ease: "easeIn" },
+        }}
+        className="w-full"
+      >
+        <Tabs defaultValue="account" className="w-full flex gap-10">
+          <TabsList className="flex flex-col h-full bg-transparent w-60 gap-5">
+            <TabsTrigger
+              value="experience"
+              className="bg-white/10 w-full py-2.5 text-white"
+            >
+              Experience
+            </TabsTrigger>
+            <TabsTrigger
+              value="education"
+              className="bg-white/10 w-full py-2.5 text-white"
+            >
+              Education
+            </TabsTrigger>
+            <TabsTrigger
+              value="skills"
+              className="bg-white/10 w-full py-2.5 text-white"
+            >
+              Skills
+            </TabsTrigger>
+            <TabsTrigger
+              value="about"
+              className="bg-white/10 w-full py-2.5 text-white"
+            >
+              About me
+            </TabsTrigger>
+          </TabsList>
+          <div className="flex-1 bg-red-400">
+            <TabsContent value="experience">
+              <p>
+                Experience ipsum dolor sit amet consectetur adipisicing elit.
+                Itaque alias dignissimos molestiae quia corporis recusandae
+                mollitia cumque earum blanditiis. At!
+              </p>
+            </TabsContent>
+            <TabsContent value="education">
+              <p>
+                Education ipsum dolor sit amet consectetur adipisicing elit.
+                Labore adipisci harum ut beatae porro, nam et sapiente ipsam
+                reprehenderit quasi.
+              </p>
+            </TabsContent>
+            <TabsContent value="skills">
+              <p>
+                Skills ipsum dolor sit amet consectetur adipisicing elit. Labore
+                adipisci harum ut beatae porro, nam et sapiente ipsam
+                reprehenderit quasi.
+              </p>
+            </TabsContent>
+            <TabsContent value="about">
+              <p>
+                About ipsum dolor sit amet consectetur adipisicing elit. Labore
+                adipisci harum ut beatae porro, nam et sapiente ipsam
+                reprehenderit quasi.
+              </p>
+            </TabsContent>
+          </div>
+        </Tabs>
+      </motion.div>
+    </Container>
+  );
 };
 
 export default ResumePage;
--- constants/index.ts
+++ constants/index.ts
@@ -12,3 +12,34 @@ export const statsData = [
   { num: 8, text: "Technologies mastered" },
   { num: 500, text: "Code commits" },
 ];
+
+export const servicesData = [
+  {
+    _id: "01",
+    title: "Web Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sequi distinctio minus esse consequatur fugit accusamus velit iste quisquam aut.",
+    href: "https://www.youtube.com/@reactjsBD",
+  },
+  {
+    _id: "02",
+    title: "App Development",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sequi distinctio minus esse consequatur fugit accusamus velit iste quisquam aut.",
+    href: "https://www.youtube.com/@reactjsBD",
+  },
+  {
+    _id: "03",
+    title: "UI/UX Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sequi distinctio minus esse consequatur fugit accusamus velit iste quisquam aut.",
+    href: "https://www.youtube.com/@reactjsBD",
+  },
+  {
+    _id: "04",
+    title: "Logo Design",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, sequi distinctio minus esse consequatur fugit accusamus velit iste quisquam aut.",
+    href: "https://www.youtube.com/@reactjsBD",
+  },
+];
