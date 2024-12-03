f61ebbc0c66200740c4ba5987e516dcc28cd9765
Noor Mohammad <noor@arogga.com.bd>
2024-12-03 16:22:48 +0600

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/Container";
+import HomeDescripton from "@/components/HomeDescripton";
 import Photo from "@/components/Photo";
 import SocialLinks from "@/components/SocialLinks";
 import Statistics from "@/components/Statistics";
@@ -19,12 +20,13 @@ export default function Home() {
               John Doe
             </h1>
           </div>
-          <p className="w-auto md:max-w-[500px] text-white/50 font-thin leading-6">
+          <HomeDescripton />
+          {/* <p className="w-auto md:max-w-[500px] text-white/50 font-thin leading-6">
             As a Software Engineer, I design and build innovative software
             solutions, solve complex problems, and ensure systems are scalable
             and user-friendly. From creating web apps to optimizing backend
             systems, I bridge the gap between technology and user needs.
-          </p>
+          </p> */}
 
           <Button className="bg-transparent rounded-full border border-lightSky/50 text-lightSky hover:bg-hoverColor hover:text-black hoverEffect">
             Download cv <Download />
